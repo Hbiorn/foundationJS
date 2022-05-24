274d70d6d1d7dc3beb2002b19f06ad58a0353db6
Ursa <haley.biorn@gmail.com>
2022-05-24 14:41:39 -0600

    Completed fundamentals part one

---
+++ javascript.js
@@ -0,0 +1,17 @@
+// tasks from javascript.info //
+!(function () {
+let admin, name;
+name = "John"
+admin = name;
+
+//alert(admin);//
+
+})()
+//my answer//
+const planet = "Earth";
+let author = "current user";
+//solution//
+let ourPlanetName = "Earth";
+//Note, we could use a shorter name planet, but it might not be obvious what planet it refers to. It’s nice to be more verbose. At least until the variable isNotTooLong.//
+let currentUserName = "John";
+//Again, we could shorten that to userName if we know for sure that the user is current. Modern editors and autocomplete make long variable names easy to write. Don’t save on them. A name with 3 words in it is fine.//
